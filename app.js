68fe4fb413331a7c38c2f5f85c96fad9e145cbfb
Prateek yadav <shubhamy9451@gmail.com>
2023-07-23 18:49:12 +0530

    initial comit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express=require('express')
+const errorMiddleware=require('./middleware/error')
+const cookieParser=require('cookie-parser');
+const bodyParser =require("body-parser");
+const fileUpload=require("express-fileupload")
+const dotenv=require('dotenv')
+const app=express();
+//   config
+dotenv.config({path:'backend/config/config.env'});
+app.use(express.json());
+app.use(cookieParser());
+// app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true }));
+app.use(fileUpload({ limits: { fileSize: 10 * 1024 * 1024 } }));
+// app.use(
+//     fileUpload({
+//       limits: { fileSize: 10 * 1024 * 1024 }, // Set the limit to 10MB
+//     })
+//   );
+
+// routes import
+const product=require('./routes/productRoute')
+const user =require('./routes/userRoute')
+const order =require('./routes/orderRoute')
+const payment =require('./routes/paymentRoute')
+
+app.use('/api/v1',product);
+app.use('/api/v1',user);
+app.use('/api/v1',order);
+app.use('/api/v1',payment);
+
+// middleware for error
+app.use(errorMiddleware);
+module.exports=app;  
+
+
+
+
